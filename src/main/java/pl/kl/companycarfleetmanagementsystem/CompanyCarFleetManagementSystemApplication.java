b344164bb1265bc8c3bb63e0f8f3a393b7c577d8
klezner <krzys3ek@gmail.com>
2021-09-01 13:45:00 +0200

    Initial commit

---
+++ src/main/java/pl/kl/companycarfleetmanagementsystem/CompanyCarFleetManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.kl.companycarfleetmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyCarFleetManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyCarFleetManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kl/companycarfleetmanagementsystem/CompanyCarFleetManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kl.companycarfleetmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyCarFleetManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
