97f6d9d78ab4786abacdc709bb75dc2a3973d747
klezner <krzys3ek@gmail.com>
2021-09-06 14:37:40 +0200

    Implement all trips from api fetching

--- src/main/java/pl/kl/companycarfleetmanagementsystem/trip/TripController.java
+++ src/main/java/pl/kl/companycarfleetmanagementsystem/trip/TripController.java
@@ -3,12 +3,12 @@ package pl.kl.companycarfleetmanagementsystem.trip;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,5 +26,22 @@ public class TripController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(tripMapper.mapTripToTripResponse(trip));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TripResponse>> getAllTrips() {
+        final List<Trip> trips = tripService.fetchAllTrips();
+
+        if (trips.size() == 0) {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body(new ArrayList<>());
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(trips.stream()
+                            .map(tripMapper::mapTripToTripResponse)
+                            .collect(Collectors.toList()));
+        }
     }
 }
