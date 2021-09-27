1cd86ce46602c89cd81063a7c2cb3f68abf33c4f
klezner <krzys3ek@gmail.com>
2021-09-27 19:48:35 +0200

    Implement editing repair

--- src/main/java/pl/kl/companycarfleetmanagementsystem/repair/RepairService.java
+++ src/main/java/pl/kl/companycarfleetmanagementsystem/repair/RepairService.java
@@ -2,12 +2,14 @@ package pl.kl.companycarfleetmanagementsystem.repair;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import pl.kl.companycarfleetmanagementsystem.trip.Trip;
 import pl.kl.companycarfleetmanagementsystem.trip.TripService;
 import pl.kl.companycarfleetmanagementsystem.validator.DateValidator;
 import pl.kl.companycarfleetmanagementsystem.validator.MeterStatusValidator;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
@@ -41,5 +43,28 @@ public class RepairService {
     public List<Repair> fetchAllRepairs() {
 
         return repairRepository.findAll();
+    }
+
+    @Transactional
+    public Repair editRepair(UpdateRepairRequest request) {
+        final Repair repair = repairRepository.findById(request.getId())
+                .orElseThrow(() -> new NoSuchElementException("Repair with id: " + request.getId() + " not found"));
+
+        final Trip trip = tripService.fetchTripById(request.getTripId());
+
+        DateValidator.validateLeftAndPickupDatesForRepair(request.getLeftDate(), request.getPickupDate());
+        DateValidator.validateDateForTripDates(request.getLeftDate(), trip.getDepartureDate(), trip.getReturnDate());
+        DateValidator.validateDateForTripDates(request.getPickupDate(), trip.getDepartureDate(), trip.getReturnDate());
+        MeterStatusValidator.validateMeterStatusForTripDates(request.getLeftMeterStatus(), trip.getDepartureMeterStatus(), trip.getReturnMeterStatus());
+
+        repair.setLeftDate(request.getLeftDate());
+        repair.setLeftMeterStatus(request.getLeftMeterStatus());
+        repair.setInvoiceNumber(request.getInvoiceNumber());
+        repair.setInvoiceDate(request.getInvoiceDate());
+        repair.setRepairCost(request.getRepairCost());
+        repair.setPickupDate(request.getPickupDate());
+        repair.setTrip(trip);
+
+        return repairRepository.save(repair);
     }
 }
