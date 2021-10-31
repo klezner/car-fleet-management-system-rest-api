ce1a429a97a857a481e1fd48f886c7eb96ff2647
klezner <krzys3ek@gmail.com>
2021-10-31 21:50:39 +0100

    Implement enabling security with profiles

--- src/main/java/pl/kl/companycarfleetmanagementsystem/config/SecurityConfig.java
+++ src/main/java/pl/kl/companycarfleetmanagementsystem/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package pl.kl.companycarfleetmanagementsystem.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -18,7 +19,7 @@ import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 @EnableWebSecurity
 @RequiredArgsConstructor
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     private final UserDetailsService userDetailsService;
 
@@ -27,30 +28,49 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
-        http.authorizeRequests()
-                .mvcMatchers("/").permitAll()
-                .mvcMatchers("/swagger-ui/**").permitAll()
-                .mvcMatchers("/h2-console/**").hasRole("ADMIN")
-                .mvcMatchers(HttpMethod.GET, "/**").hasAnyRole("USER", "ADMIN")
-                .mvcMatchers(HttpMethod.POST, "/**").hasRole("ADMIN")
-                .mvcMatchers(HttpMethod.PUT, "/**").hasRole("ADMIN")
-                .mvcMatchers(HttpMethod.DELETE, "/**").hasRole("ADMIN")
-                .anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic()
-                .and()
-                .formLogin()
-                .and()
-                .exceptionHandling()
-                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-                .and()
-                .logout()
-                .and()
-                .userDetailsService(userDetailsService);
+    @Bean
+    @Profile("NO-SEC")
+    WebSecurityConfigurerAdapter noAuth() {
+        return new WebSecurityConfigurerAdapter() {
+
+            @Override
+            protected void configure(HttpSecurity http) throws Exception {
+                http.authorizeRequests().anyRequest().permitAll();
+            }
+        };
+    }
+
+    @Bean
+    @Profile("SEC")
+    WebSecurityConfigurerAdapter basicAuth() {
+        return new WebSecurityConfigurerAdapter() {
+
+            @Override
+            protected void configure(HttpSecurity http) throws Exception {
+                http.csrf().disable();
+                http.headers().frameOptions().disable();
+                http.authorizeRequests()
+                        .mvcMatchers("/").permitAll()
+                        .mvcMatchers("/swagger-ui/**").permitAll()
+                        .mvcMatchers("/h2-console/**").hasRole("ADMIN")
+                        .mvcMatchers(HttpMethod.GET, "/**").hasAnyRole("USER", "ADMIN")
+                        .mvcMatchers(HttpMethod.POST, "/**").hasRole("ADMIN")
+                        .mvcMatchers(HttpMethod.PUT, "/**").hasRole("ADMIN")
+                        .mvcMatchers(HttpMethod.DELETE, "/**").hasRole("ADMIN")
+                        .anyRequest()
+                        .authenticated()
+                        .and()
+                        .httpBasic()
+                        .and()
+                        .formLogin()
+                        .and()
+                        .exceptionHandling()
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                        .and()
+                        .logout()
+                        .and()
+                        .userDetailsService(userDetailsService);
+            }
+        };
     }
 }
