5c9e2e26df20bf00f05383420f166a2a85a0d961
klezner <krzys3ek@gmail.com>
2021-11-14 19:54:18 +0100

    Fix SecurityConfig class

--- src/main/java/pl/kl/companycarfleetmanagementsystem/config/SecurityConfig.java
+++ src/main/java/pl/kl/companycarfleetmanagementsystem/config/SecurityConfig.java
@@ -35,8 +35,7 @@ public class SecurityConfig {
 
             @Override
             protected void configure(HttpSecurity http) throws Exception {
-//                http.authorizeRequests().anyRequest().permitAll();
-                http.authorizeRequests().antMatchers("/**").permitAll();
+                http.authorizeRequests().anyRequest().permitAll();
             }
         };
     }
